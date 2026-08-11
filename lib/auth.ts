7eb6697c82f149213221bbde2471b1543b7b757a
newly.dev <api@newly.dev>
2026-08-11 19:46:02 +0000

    chore: Point to stable production backend URL

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import * as SecureStore from "expo-secure-store";
 import { Platform } from "react-native";
 import Constants from "expo-constants";
 
-const API_URL = "https://s36v9dvx4c34kw7kb578hcj5qmnk7p6g.app.specular.dev";
+const API_URL = "https://prod-proj-auc5zpm4yhkmggajxh8se-liwg5h36mq-ey.a.run.app";
 
 export const BEARER_TOKEN_KEY = "mombrain_bearer_token";
 
